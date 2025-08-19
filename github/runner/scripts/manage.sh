adde1828cd965da5b97105c0bf2228b8b77b6869
Karl Bauer <karl.bauer@bauer-group.com>
2025-08-19 15:42:25 +0200

    fix: format manage.sh and uninstall.sh for improved readability

--- github/runner/scripts/manage.sh
+++ github/runner/scripts/manage.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 cd "$(dirname "$0")/.."
+
 export $(grep -v '^#' .env | xargs -d '\n' -I{} echo {})
+
 docker compose pull
 docker compose up -d
 docker compose up -d --scale runner="${RUNNER_COUNT}"
